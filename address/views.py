931441bda277fa50f947aa355d932113fb68bc5f
Davi Ferreira <davi.ferreira2008@hotmail.com>
2023-07-05 14:27:01 -0300

    feat: creation of user and address views

--- address/views.py
+++ address/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from .models import Address
+from .serializers import AddressSerializer
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from ..users.permissions import IsAccountOwner
 
-# Create your views here.
+class AddressView(generics.CreateAPIView):
+    serializer_class = AddressSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class AddressDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = Address.objects.all()
