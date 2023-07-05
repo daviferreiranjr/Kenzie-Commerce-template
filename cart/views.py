691c9a9cfbaaacbf043f4b1c82b23e93362d12ff
Uriel Hapuque <urihapuque11@gmail.com>
2023-07-05 14:38:09 -0300

    feat: add Cart serializer and views

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Cart
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .serializers import CartSerializer
+from .permissions import IsAdmin, IsCartOwner
 
-# Create your views here.
+
+class CartView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdmin | IsCartOwner]
+
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
