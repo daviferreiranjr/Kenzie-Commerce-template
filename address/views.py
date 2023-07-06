55e3e88fc61bf7bcbd1044d47eb46bd3b5fd4c69
Jessica Estevam <jessicafpereira16@gmail.com>
2023-07-06 14:26:31 -0300

    atualizando

--- address/views.py
+++ address/views.py
@@ -4,11 +4,13 @@ from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from ..users.permissions import IsAccountOwner
 
+
 class AddressView(generics.CreateAPIView):
     serializer_class = AddressSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
 
 class AddressDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
--- kenzie_commerce/urls.py
+++ kenzie_commerce/urls.py
@@ -20,10 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
-
     path("api/", include("orders.urls")),
-
     path("api/", include("address.urls")),
-    path("api/", include("cart.urls"))
-
+    path("api/", include("cart.urls")),
 ]
