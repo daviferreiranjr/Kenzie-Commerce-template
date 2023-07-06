556ec1a128089dfed167eb5d2033b162d55d1493
Davi Ferreira <davi.ferreira2008@hotmail.com>
2023-07-06 14:20:50 -0300

    feat: change address urls

--- address/views.py
+++ address/views.py
@@ -2,7 +2,6 @@ from .models import Address
 from .serializers import AddressSerializer
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from ..users.permissions import IsAccountOwner
 
 class AddressView(generics.CreateAPIView):
     serializer_class = AddressSerializer
@@ -12,6 +11,5 @@ class AddressView(generics.CreateAPIView):
 
 class AddressDetailView(generics.RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAccountOwner]
 
     queryset = Address.objects.all()
